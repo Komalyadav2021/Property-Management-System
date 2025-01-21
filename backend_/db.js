cd8990b5f8b908bfb860373601358b25b5effd8d
Komal Yadav <102368062+Komalyadav2021@users.noreply.github.com>
2025-01-21 21:52:04 +0530

    Update db.js

--- backend_/db.js
+++ backend_/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/PMS1", { 
+  .connect("mongodb://localhost:27017/PMS", { 
     useNewUrlParser: true,
     useUnifiedTopology: true, 
   })
