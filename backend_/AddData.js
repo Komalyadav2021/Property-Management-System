3f11f8e9fd83a9e2c954ee2a170944a2624966b3
Komal Yadav <102368062+Komalyadav2021@users.noreply.github.com>
2025-01-21 21:54:34 +0530

    Update AddData.js

--- backend_/AddData.js
+++ backend_/AddData.js
@@ -1,43 +1,61 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  phone_number: {
-    type: String,
-    required: true,
-  },
-  street_name: {
-    type: String,
-    required: true,
-  },
-  area: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  furnished_status: {
-    type: String,
-    required: true,
-  },
-  rent: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: Array,
-    required: true,
-  },
+// const schema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   phone_number: {
+//     type: String,
+//     required: true,
+//   },
+//   street_name: {
+//     type: String,
+//     required: true,
+//   },
+//   area: {
+//     type: String,
+//     required: true,
+//   },
+//   city: {
+//     type: String,
+//     required: true,
+//   },
+//   state: {
+//     type: String,
+//     required: true,
+//   },
+//   furnished_status: {
+//     type: String,
+//     required: true,
+//   },
+//   rent: {
+//     type: String,
+//     required: true,
+//   },
+//   image: {
+//     type: Array,
+//     required: true,
+//   },
+// });
+
+// const addData = new mongoose.model("addData", schema);
+// module.exports = addData;
+
+const mongoose = require('mongoose');
+
+const propertySchema = new mongoose.Schema({
+  name: String,
+  phone_number: String,
+  street_name: String,
+  city: String,
+  state: String,
+  area: String,
+  furnished_status: String,
+  rent: String,
+  image: [String], // Array to store image file paths
 });
 
-const addData = new mongoose.model("addData", schema);
+const addData = mongoose.model('Property', propertySchema);
+
 module.exports = addData;
