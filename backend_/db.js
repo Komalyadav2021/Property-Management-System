0635888ef49457d2b076699ac6ed264ded984a90
Komal Yadav <102368062+Komalyadav2021@users.noreply.github.com>
2025-01-22 12:25:15 +0530

    Update db.js

--- backend_/db.js
+++ backend_/db.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose
-  .connect("mongodb://localhost:27017/PMS1", { 
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
-  })
-  .then(() => {
-    console.log("Connected to MongoDB Compass");
-  })
-  .catch((error) => {
-    console.error("Connection error:", error);
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/PMS1", { 
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true, 
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB Compass");
+//   })
+//   .catch((error) => {
+//     console.error("Connection error:", error);
+//   });
 
-  module.exports = mongoose;
+//   module.exports = mongoose;
 
 
