70fe2e15f611f4ffac03a82d1bf84902fec460db
Komal Yadav <102368062+Komalyadav2021@users.noreply.github.com>
2025-11-02 01:28:19 +0530

    Update AddData.js

--- backend_/AddData.js
+++ backend_/AddData.js
@@ -1,46 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const schema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   phone_number: {
-//     type: String,
-//     required: true,
-//   },
-//   street_name: {
-//     type: String,
-//     required: true,
-//   },
-//   area: {
-//     type: String,
-//     required: true,
-//   },
-//   city: {
-//     type: String,
-//     required: true,
-//   },
-//   state: {
-//     type: String,
-//     required: true,
-//   },
-//   furnished_status: {
-//     type: String,
-//     required: true,
-//   },
-//   rent: {
-//     type: String,
-//     required: true,
-//   },
-//   image: {
-//     type: Array,
-//     required: true,
-//   },
-// });
-
-// const addData = new mongoose.model("addData", schema);
-// module.exports = addData;
 
 const mongoose = require('mongoose');
 
@@ -59,3 +16,4 @@ const propertySchema = new mongoose.Schema({
 const addData = mongoose.model('Property', propertySchema);
 
 module.exports = addData;
+
